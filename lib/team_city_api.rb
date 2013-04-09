48c188930270241135a29e54e69da6cb511c177f
orlando <operri@opentable.com>
2013-04-09 21:15:52 +0100

    first idea of the class api

---
+++ lib/team_city_api.rb
@@ -0,0 +1,17 @@
+class TeamCityApi
+  def initialize
+
+  end
+
+  def get_all_projects
+
+  end
+
+  def get_project_details(project_id)
+
+  end
+
+  def get_build_configuration_for_project(project_id)
+
+  end
+end
