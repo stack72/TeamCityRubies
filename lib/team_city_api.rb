64ca994c9971938f8d352a1b3b6d64d0de76f1f0
stack72 <public@github.com>
2013-04-09 22:18:56 +0100

    added supporting scaffold for project details by projectname and id. also added the ability to get buildTypes per project id

--- lib/team_city_api.rb
+++ lib/team_city_api.rb
@@ -22,7 +22,7 @@ class TeamCityApi
 
   def get_project_details_by_id(project_id)
       Net::HTTP.start('teamcity.codebetter.com',80) do |http|
-          req = Net::HTTP::Get.new('/httpAuth/app/rest/projects/id:#{project_id}')
+          req = Net::HTTP::Get.new("/httpAuth/app/rest/projects/id:#{project_id}")
           req.basic_auth(@username,@password)
           req.add_field('Accept','Application/json')
           response = http.request(req)
@@ -31,17 +31,31 @@ class TeamCityApi
   end
   
   def get_project_details_by_name(project_name)
-  
+    Net::HTTP.start('teamcity.codebetter.com',80) do |http|
+      req = Net::HTTP::Get.new("/httpAuth/app/rest/projects/name:#{project_name}")
+      req.basic_auth(@username,@password)
+      req.add_field('Accept','Application/json')
+      response = http.request(req)
+      puts response.body
+    end
   end
 
   def get_build_configuration_for_project(project_id)
-
+    Net::HTTP.start('teamcity.codebetter.com',80) do |http|
+      req = Net::HTTP::Get.new("/httpAuth/app/rest/projects/id:#{project_id}/buildTypes")
+      req.basic_auth(@username,@password)
+      req.add_field('Accept','Application/json')
+      response = http.request(req)
+      puts response.body
+    end
   end
 end
 
 #to test this class
-#api = TeamCityApi.new
-#api.authenticate('teamcitysharpuser','qwerty')
+api = TeamCityApi.new
+api.authenticate('teamcitysharpuser','qwerty')
 
 #api.get_all_projects
 #api.get_project_details_by_id('project115')
+#api.get_project_details_by_name('easyhttp')
+api.get_build_configuration_for_project('project115')
